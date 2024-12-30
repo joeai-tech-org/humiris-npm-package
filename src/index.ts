4bce51ea836a77ff17d8019566709ee9bddadb60
vilevo <hilexpertiz@gmail.com>
2024-12-30 14:01:48 +0000

    add stream

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,42 @@
-type Role = 'user' | 'assistant' | 'system';
+type Role = "user" | "assistant" | "system";
 
 type BasicChatOutput = {
-    content: string;
-    metadata?: {
-        usage: {
-            input_tokens: number;
-            output_tokens: number;
-            total_tokens: number;
-        };
-        llm: {
-            name: string;
-            version: string;
-        };
+  content: string;
+  metadata?: {
+    usage: {
+      input_tokens: number;
+      output_tokens: number;
+      total_tokens: number;
     };
+    llm: {
+      name: string;
+      version: string;
+    };
+  };
 };
 
 type AdvancedChatOutput = {
-    content: string;
-    metadata?: {
-        usage: {
-            input_tokens: number;
-            output_tokens: number;
-            total_tokens: number;
-        };
+  content: string;
+  metadata?: {
+    usage: {
+      input_tokens: number;
+      output_tokens: number;
+      total_tokens: number;
     };
-}
+  };
+};
 
 interface MessageType {
   role: Role;
   content: string;
 }
 
-
 interface basicPayload {
   mixTuningId: string;
   messages: MessageType[];
   temperature: number;
   maxTokens: number;
-  stream: boolean;
+  stream?: boolean;
 }
 
 interface advancedPayload {
@@ -52,22 +51,32 @@ class BasicMoAi {
   private apiKey: string;
 
   constructor(apiKey: string) {
-    this.apiUrl = 'https://moai-service-app.humiris.ai/api/api-key-operators/use-basic-mixtuning';
+    this.apiUrl =
+      "https://moai-service-app.humiris.ai/api/api-key-operators/use-basic-mixtuning";
     this.apiKey = apiKey;
   }
 
-  public async useBasicMixtuning(payload: basicPayload): Promise<BasicChatOutput> {
+  public async useBasicMixtuning(
+    payload: basicPayload
+  ): Promise<BasicChatOutput> {
     try {
-       const response = await fetch(this.apiUrl, {
-      method: 'POST',
-      headers: {
-        'moai-api-key': this.apiKey,
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(payload)
-    });
-    const responseData = await response.json();
-    return  responseData;
+      const response = await fetch(this.apiUrl, {
+        method: "POST",
+        headers: {
+          "moai-api-key": this.apiKey,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
+      let responseData;
+
+      if (payload.stream == true) {
+        responseData = response;
+      } else {
+        responseData = await response.json();
+      }
+
+      return responseData;
     } catch (error) {
       throw new Error(`Failed to send request: ${error}`);
     }
@@ -79,26 +88,29 @@ class AdvancedMoAi {
   private apiKey: string;
 
   constructor(apiKey: string) {
-    this.apiUrl = 'https://moai-service-app.humiris.ai/api/api-key-operators/use-advanced-mixtuning';
+    this.apiUrl =
+      "https://moai-service-app.humiris.ai/api/api-key-operators/use-advanced-mixtuning";
     this.apiKey = apiKey;
   }
 
-  public async useAdvancedMixtuning(payload: advancedPayload): Promise<AdvancedChatOutput> {
+  public async useAdvancedMixtuning(
+    payload: advancedPayload
+  ): Promise<AdvancedChatOutput> {
     try {
-       const response = await fetch(this.apiUrl, {
-      method: 'POST',
-      headers: {
-        'moai-api-key': this.apiKey,
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(payload)
-    });
-    const responseData = await response.json();
-    return responseData;
+      const response = await fetch(this.apiUrl, {
+        method: "POST",
+        headers: {
+          "moai-api-key": this.apiKey,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
+      const responseData = await response.json();
+      return responseData;
     } catch (error) {
       throw new Error(`Failed to send request: ${error}`);
     }
   }
 }
 
-export { BasicMoAi, AdvancedMoAi, BasicChatOutput,  AdvancedChatOutput};
+export { BasicMoAi, AdvancedMoAi, BasicChatOutput, AdvancedChatOutput };
